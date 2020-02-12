2ea253859131dd4d208d645e92ac4d903ff4e839
Aaditya <aadi123@cs.utexas.edu>
2020-02-12 11:22:23 -0600

    removed distance travelled cout from code

--- src/navigation/navigation.cc
+++ src/navigation/navigation.cc
@@ -116,7 +116,6 @@ void Navigation::Run() {
   // using odometry calculations for 1-D TOC
   msg.velocity = toc->getVelocity(distance_travelled, current_speed);
   msg.curvature = toc->getCurvature();
-  std::cout<<distance_travelled<<std::endl;
   drive_pub_.publish(msg);
 }
 
