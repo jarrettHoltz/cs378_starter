a0a3a3f02a174e460688bf7d15550e0286104ce1
Aaditya Murthy <amurthy123@gmail.com>
2020-02-24 01:51:47 -0600

    changed weights

--- src/navigation/navigation.h
+++ src/navigation/navigation.h
@@ -120,8 +120,8 @@ class Navigation {
 
   // max clearance and weights for scoring function
   static constexpr float max_clearance = 0.15;
-  static constexpr float w1 = 1.0;
-  static constexpr float w2 = 0.0;
+  static constexpr float w1 = 3.0;
+  static constexpr float w2 = -3.0;
   // 1-D TOC
   Controller* toc;
 };
